6c966102d132d3f7adbf1d519e48dbb2ea3863bc
lingdong <1003762842@qq.com>
2020-06-27 23:03:59 +0800

    初始化

--- src/main/java/com/gw/FileChoseApplication.java
+++ src/main/java/com/gw/FileChoseApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileChoseApplication {
 
+    //ceshi
     public static void main(String[] args) {SpringApplication.run(FileChoseApplication.class, args);
     }
 

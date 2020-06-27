462c573e5ec84c885e8feeb7badba7fe43497620
lingdong <1003762842@qq.com>
2020-06-27 23:18:45 +0800

    gouwei_dev

--- src/main/java/com/gw/FileChoseApplication.java
+++ src/main/java/com/gw/FileChoseApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class FileChoseApplication {
 
+            //ge_dev
        public static void main(String[] args) {SpringApplication.run(FileChoseApplication.class, args);
     }
 
